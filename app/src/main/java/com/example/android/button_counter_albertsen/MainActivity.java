049cc1e9f2b1ddfe041c6cde61834878d3798e24
Noah Albertsen <albertsennk@g.cofc.edu>
2019-02-04 17:34:32 -0500

    Added functionality for buttons, still working out saving state between screen rotations

--- app/src/main/java/com/example/android/button_counter_albertsen/MainActivity.java
+++ app/src/main/java/com/example/android/button_counter_albertsen/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.android.button_counter_albertsen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private Button inc;
+    private Button dec;
+    private Button res;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.incrementButton);
+        dec = findViewById(R.id.decrementButton);
+        res = findViewById(R.id.resetButton);
+        test = findViewById(R.id.counter);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                test.setText(Integer.toString(count));
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                test.setText(Integer.toString(count));
+            }
+        });
+
+        res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                test.setText(Integer.toString(count));
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
